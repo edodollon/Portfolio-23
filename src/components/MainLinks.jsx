2beca5dc2cf15ee9eda25c60277872ea4f1d7fd0
Jomel Edodollon <jomeledodollon@yahoo.ca>
2023-01-16 15:20:06 -0800

    main page minimum build

---
+++ src/components/MainLinks.jsx
@@ -0,0 +1,17 @@
+import "../styles/mainLinks.scss";
+
+export default function MainLinks() {
+  return (
+    <div className="nav">
+      <div>
+        <a href="">WORKS</a>
+      </div>
+      <div>
+        <a href="">ABOUT</a>
+      </div>
+      <div>
+        <a href="">CONTACT</a>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/NameLogo.jsx
@@ -0,0 +1,10 @@
+import "../styles/nameLogo.scss";
+
+export default function NameLogo() {
+  return (
+    <a className="logo" href="">
+      <span className="firstName">JOMEL</span>
+      <span className="lastName">EDODOLLON</span>
+    </a>
+  );
+}
