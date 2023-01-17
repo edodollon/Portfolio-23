ada17dafdf28961843abfbbc0fb39960940b22eb
Jomel Edodollon <jomeledodollon@yahoo.ca>
2023-01-16 19:17:10 -0800

    Adds initial root route and basic error page

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./routes/ErrorPage";
+
 import App from "./App";
 <style>
   @import
   url('https://fonts.googleapis.com/css2?family=Inter:wght@400;800;900&display=swap');
 </style>;
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
