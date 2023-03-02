23bde41d9cd852f243404684ea7226f3ccd1953d
Jomel Edodollon <jomeledodollon@yahoo.ca>
2023-03-01 19:34:45 -0800

    initial mobile styling

--- src/components/NameLogo.jsx
+++ src/components/NameLogo.jsx
@@ -2,9 +2,14 @@ import "../styles/nameLogo.scss";
 
 export default function NameLogo() {
   return (
-    <a className="logo" href="">
-      <span className="firstName">JOMEL</span>
-      <span className="lastName">EDODOLLON</span>
-    </a>
+    <>
+      <a className="logo" href="">
+        <span className="firstName">JOMEL</span>
+        <span className="lastName">EDODOLLON</span>
+      </a>
+      <p className="blurb">
+        2023 Portfolio of an aspiring web developer based in Vancouver
+      </p>
+    </>
   );
 }
