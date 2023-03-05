6e7b4f8a83e59df746f4de44358aaca738e863b4
Jomel Edodollon <jomeledodollon@yahoo.ca>
2023-03-04 19:50:21 -0800

    About page content and initial styling

--- src/routes/AboutPage.jsx
+++ src/routes/AboutPage.jsx
@@ -1,5 +1,57 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import github_icon from "../assets/github_icon_48px.png";
+import html_icon from "../assets/html5_icon_48px.png";
+import css_icon from "../assets/css3_icon_48px.png";
+import git_icon from "../assets/git_icon_48px.png";
+import react_icon from "../assets/react_icon_48px.png";
+import next_icon from "../assets/nextjs_icon_48px.png";
+import "../styles/aboutPage.scss";
 
 export default function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <div className="about-wrapper">
+      <Link to={"/"}>Home</Link>
+      <h1>ABOUT ME</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam
+        incidunt suscipit similique eum tenetur sint inventore consequatur
+        dolores expedita commodi, eveniet repudiandae architecto, dolorum nisi
+        omnis doloremque! Quis, totam consequatur. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Illum magni cupiditate vel consequatur
+        nesciunt autem consectetur iste asperiores, repellendus ullam animi
+        voluptatibus reprehenderit officia ad.
+      </p>
+
+      <h2>SKILLS</h2>
+      <div className="skills-wrapper">
+        <div className="skill-container">
+          <p>HTML5</p>
+          <img src={html_icon} alt="html icon" />
+        </div>
+        <div className="skill-container">
+          <p>CSS3</p>
+          <img src={css_icon} alt="css icon" />
+        </div>
+        <div className="skill-container">
+          <p>React</p>
+          <img src={react_icon} alt="html icon" />
+        </div>
+        <div className="skill-container">
+          <p>Next.js</p>
+          <img src={next_icon} alt="html icon" />
+        </div>
+        <div className="skill-container">
+          <p>Github</p>
+          <img src={github_icon} alt="github icon" />
+        </div>
+        <div className="skill-container">
+          <p>Git</p>
+          <img src={git_icon} alt="git icon" />
+        </div>
+      </div>
+
+      <button className="resume-btn">RESUME</button>
+    </div>
+  );
 }
