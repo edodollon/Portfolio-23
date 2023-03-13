40807aab2caf0ab0afda13dcbf121aafce891ebf
Jomel Edodollon <jomeledodollon@yahoo.ca>
2023-03-12 23:06:01 -0700

    create additional project pages with routes

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import ErrorPage from "./routes/ErrorPage";
 import App from "./App";
 import AboutPage from "./routes/AboutPage";
 import PokemonLibrary from "./routes/PokemonLibrary";
+import StoragePage from "./routes/StoragePage";
+import ClydePage from "./routes/ClydePage";
 <style>
   @import
   url('https://fonts.googleapis.com/css2?family=Inter:wght@400;800;900&display=swap');
@@ -24,6 +26,14 @@ const router = createBrowserRouter([
   {
     path: "The-Pokemon-Library",
     element: <PokemonLibrary />,
+  },
+  {
+    path: "Storage-App",
+    element: <StoragePage />,
+  },
+  {
+    path: "Clyde-Portfolio",
+    element: <ClydePage />,
   },
 ]);
 
---
+++ src/routes/ClydePage.jsx
@@ -0,0 +1,34 @@
+import "../styles/projectPage.scss";
+
+export default function ClydePage() {
+  return (
+    <div className="project-wrapper">
+      <section className="image-container">
+        <img src="" alt="" />
+      </section>
+      <section className="project-info-wrapper">
+        <div className="project-content-wrapper">
+          <div className="project-info-header">
+            <p>Personal project</p>
+            <p>2022</p>
+          </div>
+          <h1 className="project-heading">Project Heading</h1>
+          <div className="project-description-wrapper">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              voluptas assumenda quisquam temporibus dolore! Libero, recusandae
+              est necessitatibus ducimus maiores tenetur cupiditate error fugiat
+              molestias.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+              repellat dolore soluta, voluptatum maxime nemo libero provident
+              possimus consectetur, cupiditate dicta neque numquam beatae. Neque
+              quia corporis explicabo libero iusto.
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
