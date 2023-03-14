e18fce149f7720b43e7b7fe129b89a2b912dcbb2
Jomel Edodollon <jomeledodollon@yahoo.ca>
2023-03-13 22:22:20 -0700

    project page responsive media queries

--- src/routes/PokemonLibrary.jsx
+++ src/routes/PokemonLibrary.jsx
@@ -12,21 +12,23 @@ export default function PokemonPage() {
             <p>Personal project</p>
             <p>2022</p>
           </div>
-          <h1 className="project-heading">Project Heading</h1>
-          <div className="project-description-wrapper">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-              voluptas assumenda quisquam temporibus dolore! Libero, recusandae
-              est necessitatibus ducimus maiores tenetur cupiditate error fugiat
-              molestias.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-              repellat dolore soluta, voluptatum maxime nemo libero provident
-              possimus consectetur, cupiditate dicta neque numquam beatae. Neque
-              quia corporis explicabo libero iusto.
-            </p>
-          </div>
+          <section className="project-info-section">
+            <h1 className="project-heading">Project Heading</h1>
+            <div className="project-description-wrapper">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+                voluptas assumenda quisquam temporibus dolore! Libero,
+                recusandae est necessitatibus ducimus maiores tenetur cupiditate
+                error fugiat molestias.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+                repellat dolore soluta, voluptatum maxime nemo libero provident
+                possimus consectetur, cupiditate dicta neque numquam beatae.
+                Neque quia corporis explicabo libero iusto.
+              </p>
+            </div>
+          </section>
         </div>
       </section>
     </div>
