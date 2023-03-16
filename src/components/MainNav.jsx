db2b6c1daa9c477fd1abbef92753f26a99434c77
Jomel Edodollon <jomeledodollon@yahoo.ca>
2023-03-15 20:30:09 -0700

    Add nav component to projects and update router links

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,11 +1,20 @@
-import "../styles/mainNav.scss";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import "../styles/mainNav.scss";
 
 export default function MainNav() {
   return (
     <nav className="nav-wrapper">
       <div className="nav-items-wrapper">
-        <Link className="nav-button">Home</Link>
+        <Link to={"/"} className="nav-button">
+          Home
+        </Link>
+        <Link to={"../About"} className="nav-button">
+          About
+        </Link>
+        <Link to={"../Contact"} className="nav-button">
+          Contact
+        </Link>
       </div>
     </nav>
   );
--- src/routes/StoragePage.jsx
+++ src/routes/StoragePage.jsx
@@ -1,36 +1,40 @@
+import MainNav from "../components/MainNav";
 import "../styles/projectPage.scss";
 
 export default function StoragePage() {
   return (
-    <div className="project-wrapper">
-      <section className="image-container">
-        <img src="" alt="" />
-      </section>
-      <section className="project-info-wrapper">
-        <div className="project-content-wrapper">
-          <div className="project-info-header">
-            <p>Personal project</p>
-            <p>2023</p>
-          </div>
-          <section className="project-info-section">
-            <h1 className="project-heading">StoreMe</h1>
-            <div className="project-description-wrapper">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-                voluptas assumenda quisquam temporibus dolore! Libero,
-                recusandae est necessitatibus ducimus maiores tenetur cupiditate
-                error fugiat molestias.
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-                repellat dolore soluta, voluptatum maxime nemo libero provident
-                possimus consectetur, cupiditate dicta neque numquam beatae.
-                Neque quia corporis explicabo libero iusto.
-              </p>
+    <>
+      <MainNav />
+      <div className="project-wrapper">
+        <section className="image-container">
+          <img src="" alt="" />
+        </section>
+        <section className="project-info-wrapper">
+          <div className="project-content-wrapper">
+            <div className="project-info-header">
+              <p>Personal project</p>
+              <p>2023</p>
             </div>
-          </section>
-        </div>
-      </section>
-    </div>
+            <section className="project-info-section">
+              <h1 className="project-heading">StoreMe</h1>
+              <div className="project-description-wrapper">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Nostrum voluptas assumenda quisquam temporibus dolore! Libero,
+                  recusandae est necessitatibus ducimus maiores tenetur
+                  cupiditate error fugiat molestias.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Fugiat repellat dolore soluta, voluptatum maxime nemo libero
+                  provident possimus consectetur, cupiditate dicta neque numquam
+                  beatae. Neque quia corporis explicabo libero iusto.
+                </p>
+              </div>
+            </section>
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
